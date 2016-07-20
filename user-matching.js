5a6e618d3094b454c1c118e9545f43dd488d6c34
Yann Feunteun <yannfeunteun@gmail.com>
2016-07-20 13:53:06 +0000

    Add implementation  for user_ids_from_interests

--- user-matching.js
+++ user-matching.js
@@ -1,3 +1,5 @@
+var _ = require("underscore");
+
 function interests_by_user_id(id, users) {
 
   userForId = users.filter(function(user) {
@@ -5,11 +7,20 @@ function interests_by_user_id(id, users) {
   });
 
   if(userForId.length == 1) {
-     return (userForId[0].interests);
+     return (JSON.parse(userForId[0].interests));
   } else {
     return ([]);
   }
 }
 
+function user_ids_by_interest(topic, users) {
+  return(
+    _.map(
+      _.filter(users, function(user) {
+        return _.contains(JSON.parse(user.interests), topic)
+      })
+   , function(entry) {return entry.id;}));
+}
 
 exports.interests_by_user_id = interests_by_user_id;
+exports.user_ids_by_interest = user_ids_by_interest;
