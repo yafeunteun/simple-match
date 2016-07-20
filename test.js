8e8ef003a1e5c5967b7a6cb9d1ac4d8f657833f0
Yann Feunteun <yannfeunteun@gmail.com>
2016-07-20 09:57:08 +0000

    Fix test for interests_by_user_id

--- test.js
+++ test.js
@@ -1,8 +1,9 @@
 var assert = require('assert');
+var userMatching = require('./user-matching');
 
 describe('Returns the interests given a user id', function() {
   it('Returns the correct set of interests for id=3', function(){
-    assert.deepEqual(interests_by_user_id(3), ["statistics", "regression", "probability"])
+    assert.deepEqual(userMatching.interests_by_user_id(3), ["statistics", "regression", "probability"])
   });
 });
 
